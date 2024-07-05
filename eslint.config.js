40bb5258a42acf7bdbcfd26ce1212fcfc6707233
Nastya Gamza <nastyagamza2000@gmail.com>
2024-07-05 23:16:09 +0300

    refactor: update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -5,8 +5,6 @@ import eslintReact from 'eslint-plugin-react';
 import eslintPluginReactCompiler from 'eslint-plugin-react-compiler';
 import eslintReactRefresh from 'eslint-plugin-react-refresh';
 import prettierPlugin from 'eslint-plugin-prettier';
-import eslintConfigPrettier from 'eslint-config-prettier';
-import babelParser from '@babel/eslint-parser';
 import tsParser from '@typescript-eslint/parser';
 
 export default tsEslint.config(
@@ -17,7 +15,7 @@ export default tsEslint.config(
       'react-refresh': eslintReactRefresh,
       'react-compiler': eslintPluginReactCompiler,
       prettier: prettierPlugin,
-      parser: babelParser,
+      parser: tsParser,
     },
   },
   {
@@ -40,16 +38,9 @@ export default tsEslint.config(
   {
     files: ['**/*.{ts,tsx}'],
     rules: {
-      // ...prettierPlugin.configs.recommended.rules,
-      // ...eslintConfigPrettier.rules,
       '@typescript-eslint/no-explicit-any': 'error',
       'react-compiler/react-compiler': 'error',
       'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
-      'prefer-const': 'error',
-      // 'react/jsx-curly-brace-presence': ['warn', { props: 'never', children: 'never' }],
-      // 'react/function-component-definition': ['warn', { namedComponents: 'arrow-function' }],
-      // 'react/self-closing-comp': ['error', { component: true, html: true }],
-      // 'max-lines': ['warn', { max: 124 }],
     },
   },
 );
