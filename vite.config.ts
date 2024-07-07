7a2fb352cfeb33e9a90f7450b728382ee5003899
Nastya Gamza <nastyagamza2000@gmail.com>
2024-07-07 16:24:03 +0300

    fix: vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,7 @@ import { defineConfig } from 'vite';
 import svgr from 'vite-plugin-svgr';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
+  base: command === 'build' ? '/rss-react/' : '/',
   plugins: [react(), svgr()],
-  base: '/rss-react/',
-});
+}));
