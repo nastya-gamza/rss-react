a12fadc62742029ad59095dd15764e2e428e6a6f
Nastya Gamza <nastyagamza2000@gmail.com>
2024-07-24 00:24:52 +0300

    feat: add tests for rtk slices

---
+++ src/store/tests/selected-character-sice.test.tsx
@@ -0,0 +1,23 @@
+import selectedCharacterReducer, {
+  setSelectedCharacter,
+} from '../slices/selected-character-sice.ts';
+import { mockCharacter } from '../../__mocks__/characters.ts';
+
+const initialState = {
+  character: null,
+};
+
+describe('selectedCharacter slice', () => {
+  test('should return the initial state', () => {
+    const result = selectedCharacterReducer(undefined, { type: '' });
+
+    expect(result).toEqual(initialState);
+  });
+
+  test('should handle setSelectedCharacter', () => {
+    const action = { type: setSelectedCharacter.type, payload: mockCharacter };
+    const result = selectedCharacterReducer(initialState, action);
+
+    expect(result.character).toEqual(mockCharacter);
+  });
+});
